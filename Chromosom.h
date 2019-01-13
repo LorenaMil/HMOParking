6f4c385fef35895158fd69cb88f98c3ed40bd3ad
Luka Jukić <23559454+lukaj1795@users.noreply.github.com>
2019-01-13 11:39:32 +0100

    Some work done on mutation not finished yet.

--- Chromosom.h
+++ Chromosom.h
@@ -13,15 +13,18 @@ class Chromosom {
 		
 		void mutate() {
 			///select random line
-			auto line = chromosom_representation[rand() % chromosom_representation.size()];
+			auto line_index = rand() % chromosom_representation.size();
+			auto line = chromosom_representation[line_index];
 			///select random car
 			auto index1 = rand() % line.cars.size();
 			auto car = line.cars[index1];
 
 			///select line of same type but not the same line
-			auto line2=chromosom_representation[rand()&chromosom_representation.size()];
+			auto line_index2 = rand()&chromosom_representation.size();
+			auto line2=chromosom_representation[line_index2];
 			while (line2.type != line.type) {
-				line2 = chromosom_representation[rand()&chromosom_representation.size()];
+				line_index2 = rand()&chromosom_representation.size();
+				line2 = chromosom_representation[line_index2];
 			}
 			///select car from line 2
 			auto index2 = rand() % line2.cars.size();
@@ -29,8 +32,31 @@ class Chromosom {
 
 			///check cond 8
 			auto block = block_list();
+			bool cond8 = false;
 			///todo check cond 8
+			for (auto blocking : block) {
+				auto blocked_lines = blocking.blockedLines;
+				auto line1_in_blocked_lines = std::find(blocked_lines.begin(), blocked_lines.end(), line_index) != blocked_lines.end();
+				auto line2_in_blocked_lines = std::find(blocked_lines.begin(), blocked_lines.end(), line_index2) != blocked_lines.end();
+				///line 1 not in blocked lines
+				if (!line1_in_blocked_lines) 
+				{
+					///line 2 not in blocked lines
+					if (line2_in_blocked_lines) {
+						cond8 = true;
+						continue;
+					}
+				}
+				else {
+					auto blocking_line = blocking.blockingLine;
+					auto bl = chromosom_representation[blocking_line];
 
+					///last car in blocking line gets out before first car in line 1
+					bool timecheck1 = bl.cars[bl.cars.size()].time < chromosom_representation[line_index].cars[0].time;
+					
+
+				}
+			}
 			///swap them
 			line.cars[index1] = car2;
 			line2.cars[index2] = car;
@@ -39,14 +65,14 @@ class Chromosom {
 			///sort cars
 			line.cars = sortCarsByTime(line.cars);
 			line2.cars = sortCarsByTime(line2.cars);
-
+			evaluation();
 		}
 		static Chromosom GetChild(const Chromosom &, const Chromosom &);
 		static Chromosom Random();
 		
 		double evaluation() {
-
-			//return secondGoal(chromosom_representation)/(firstGoal(chromosom_representation)+.0001);
+			fitness = secondGoal(chromosom_representation) / (firstGoal(chromosom_representation) + .0001);
+			return fitness;
 		}
 
 		bool operator<(const Chromosom & rhs) const { return fitness < rhs.fitness; }
